dba429866bb6bac8974419823179a8ffb875837b
Jakub Porebski <kubusb@gmail.com>
2020-09-21 20:04:47 +0200

    Bugs and indent corrected

--- src/todo/views.py
+++ src/todo/views.py
@@ -12,12 +12,12 @@ class TodoItemViewSet(viewsets.ModelViewSet):
     queryset = TodoItem.objects.all()
     serializer_class = TodoItemSerializer
 
-        def perform_create(self, serializer):
-            # Save instance to get primary key and then update URL
-            instance = serializer.save()
-            instance.url = reverse('todoitem-detail', agrs=[instance.pk], request=self.request)
-            instane.save()
+    def perform_create(self, serializer):
+        # Save instance to get primary key and then update URL
+        instance = serializer.save()
+        instance.url = reverse('todoitem-detail', agrs=[instance.pk], request=self.request)
+        instane.save()
 
-        def delete(self, request):
-            TodoItem.objects.all().delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request):
+        TodoItem.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
