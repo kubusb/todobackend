b553243dfed69a539e5f5478549b84238f552d13
Jakub Porebski <kubusb@gmail.com>
2020-11-07 13:00:54 +0100

    Race condition removed with Ansible

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,8 +2,10 @@
 # Activate virtual environemnt
 . /appenv/bin/activate
 
+# Download requirements to build cache
+pip download -d /build -r requirements_test.txt --no-input
 # Install application test requirements
-pip install -r requirements_test.txt
+pip install --no-index -f /build -r requirements_test.txt
 
 # Run test.sh arguments
 exec $@
